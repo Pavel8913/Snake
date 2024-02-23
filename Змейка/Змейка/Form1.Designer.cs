356c322107817748bd0fa1c08ab902dda5521455
Pavel <pavel.en00@mail.ru>
2024-02-23 16:03:37 +0300

    Add files via upload

---
+++ Змейка/Змейка/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Змейка
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.играToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.начатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.паузаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сложностьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.легкоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.среднеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.тяжелоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.цветоваяСхемаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.холодныеЦветаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.тёплыеЦветаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.чёрнобелыеЦветаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.правилаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.game_timer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.играToolStripMenuItem,
+            this.сложностьToolStripMenuItem,
+            this.цветоваяСхемаToolStripMenuItem,
+            this.правилаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(376, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // играToolStripMenuItem
+            // 
+            this.играToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.начатьToolStripMenuItem,
+            this.паузаToolStripMenuItem,
+            this.закрытьToolStripMenuItem});
+            this.играToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_контроллер_30;
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.играToolStripMenuItem.Text = "Игра";
+            // 
+            // начатьToolStripMenuItem
+            // 
+            this.начатьToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_воспроизведение_30;
+            this.начатьToolStripMenuItem.Name = "начатьToolStripMenuItem";
+            this.начатьToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.начатьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.начатьToolStripMenuItem.Text = "Начать";
+            this.начатьToolStripMenuItem.Click += new System.EventHandler(this.начатьToolStripMenuItem_Click);
+            // 
+            // паузаToolStripMenuItem
+            // 
+            this.паузаToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_пауза_в_кружке_30;
+            this.паузаToolStripMenuItem.Name = "паузаToolStripMenuItem";
+            this.паузаToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
+            this.паузаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.паузаToolStripMenuItem.Text = "Пауза";
+            this.паузаToolStripMenuItem.Click += new System.EventHandler(this.паузаToolStripMenuItem_Click);
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_macos_закрыть_30;
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.закрытьToolStripMenuItem.Text = "Закрыть";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.закрытьToolStripMenuItem_Click);
+            // 
+            // сложностьToolStripMenuItem
+            // 
+            this.сложностьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.легкоToolStripMenuItem,
+            this.среднеToolStripMenuItem,
+            this.тяжелоToolStripMenuItem});
+            this.сложностьToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_производительность_32;
+            this.сложностьToolStripMenuItem.Name = "сложностьToolStripMenuItem";
+            this.сложностьToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.сложностьToolStripMenuItem.Text = "Сложность";
+            // 
+            // легкоToolStripMenuItem
+            // 
+            this.легкоToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_черепаха_30;
+            this.легкоToolStripMenuItem.Name = "легкоToolStripMenuItem";
+            this.легкоToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.легкоToolStripMenuItem.Text = "Легко";
+            this.легкоToolStripMenuItem.Click += new System.EventHandler(this.легкоToolStripMenuItem_Click);
+            // 
+            // среднеToolStripMenuItem
+            // 
+            this.среднеToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_кролик_30;
+            this.среднеToolStripMenuItem.Name = "среднеToolStripMenuItem";
+            this.среднеToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.среднеToolStripMenuItem.Text = "Средне";
+            this.среднеToolStripMenuItem.Click += new System.EventHandler(this.среднеToolStripMenuItem_Click);
+            // 
+            // тяжелоToolStripMenuItem
+            // 
+            this.тяжелоToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_собака_прыгает_30;
+            this.тяжелоToolStripMenuItem.Name = "тяжелоToolStripMenuItem";
+            this.тяжелоToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.тяжелоToolStripMenuItem.Text = "Тяжело";
+            this.тяжелоToolStripMenuItem.Click += new System.EventHandler(this.тяжелоToolStripMenuItem_Click);
+            // 
+            // цветоваяСхемаToolStripMenuItem
+            // 
+            this.цветоваяСхемаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.холодныеЦветаToolStripMenuItem,
+            this.тёплыеЦветаToolStripMenuItem,
+            this.чёрнобелыеЦветаToolStripMenuItem});
+            this.цветоваяСхемаToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_выбор_цвета_48;
+            this.цветоваяСхемаToolStripMenuItem.Name = "цветоваяСхемаToolStripMenuItem";
+            this.цветоваяСхемаToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.цветоваяСхемаToolStripMenuItem.Text = "Цветовая схема";
+            // 
+            // холодныеЦветаToolStripMenuItem
+            // 
+            this.холодныеЦветаToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_пипетка_64;
+            this.холодныеЦветаToolStripMenuItem.Name = "холодныеЦветаToolStripMenuItem";
+            this.холодныеЦветаToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.холодныеЦветаToolStripMenuItem.Text = "Холодные цвета";
+            this.холодныеЦветаToolStripMenuItem.Click += new System.EventHandler(this.холодныеЦветаToolStripMenuItem_Click);
+            // 
+            // тёплыеЦветаToolStripMenuItem
+            // 
+            this.тёплыеЦветаToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_пипетка_64__1_;
+            this.тёплыеЦветаToolStripMenuItem.Name = "тёплыеЦветаToolStripMenuItem";
+            this.тёплыеЦветаToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.тёплыеЦветаToolStripMenuItem.Text = "Тёплые цвета";
+            this.тёплыеЦветаToolStripMenuItem.Click += new System.EventHandler(this.тёплыеЦветаToolStripMenuItem_Click);
+            // 
+            // чёрнобелыеЦветаToolStripMenuItem
+            // 
+            this.чёрнобелыеЦветаToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_пипетка_64__2_;
+            this.чёрнобелыеЦветаToolStripMenuItem.Name = "чёрнобелыеЦветаToolStripMenuItem";
+            this.чёрнобелыеЦветаToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.чёрнобелыеЦветаToolStripMenuItem.Text = "Чёрно-белые цвета";
+            this.чёрнобелыеЦветаToolStripMenuItem.Click += new System.EventHandler(this.чёрнобелыеЦветаToolStripMenuItem_Click);
+            // 
+            // правилаToolStripMenuItem
+            // 
+            this.правилаToolStripMenuItem.Image = global::Змейка.Properties.Resources.icons8_задать_вопрос_30;
+            this.правилаToolStripMenuItem.Name = "правилаToolStripMenuItem";
+            this.правилаToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.правилаToolStripMenuItem.Text = "Правила";
+            this.правилаToolStripMenuItem.Click += new System.EventHandler(this.правилаToolStripMenuItem_Click);
+            // 
+            // game_timer
+            // 
+            this.game_timer.Interval = 200;
+            this.game_timer.Tick += new System.EventHandler(this.game_timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 357);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Змейка";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem играToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem начатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem паузаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сложностьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem легкоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem среднеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem тяжелоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem цветоваяСхемаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem холодныеЦветаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem тёплыеЦветаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem чёрнобелыеЦветаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem правилаToolStripMenuItem;
+        private System.Windows.Forms.Timer game_timer;
+    }
+}
+
---
+++ Змейка/Змейка/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Змейка
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            generate_fruit();
+            generate_grad();
+            this.Size = new Size(500, 480);
+            
+            game_timer.Interval = 180;
+
+            this.Text = "Счёт:" + " " + score;
+
+            for (int i = 0; i < scale * scale; i++)
+            {
+                snake[i] = new PictureBox();
+                snake[i].Size = new Size(scale, scale);
+                snake[i].BackColor = snake_color;
+                snake[i].Tag = "snake";
+            }
+            snake[0].Tag = "snake1";
+            snake[0].Location = new Point(200, 200);
+            this.Controls.Add(snake[0]);
+        }
+        #region переменные
+        bool goleft = true, goright = false, goup = false, godown = false;
+        int scale = 20;
+        int width = 500;
+        int height = 400;
+        int indent = 40;
+
+        int dirX, dirY;
+
+        PictureBox fruit = new PictureBox();
+        PictureBox[] gradx = new PictureBox[21];
+        PictureBox[] grady = new PictureBox[21];
+
+        int score = 0;
+
+        
+        PictureBox[] snake = new PictureBox[400];
+
+        Color snake_color = Color.Lime;
+        Color fruit_color=Color.Red;
+
+
+        #endregion;
+    
+
+        #region управление
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up )
+            {
+                goup = true;
+                godown = false;
+                goleft = false;
+                goright = false;
+            }
+            if (e.KeyCode == Keys.Down )
+            {
+                goup = false;
+                godown = true;
+                goleft = false;
+                goright = false;
+            }
+            if (e.KeyCode == Keys.Left )
+            {
+                goup = false;
+                godown = false;
+                goleft = true;
+                goright = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goup = false;
+                godown = false;
+                goleft = false;
+                goright = true;
+            }
+        }
+
+        #endregion;
+
+        private void _eatItself()
+        {
+            for (int _i = 1; _i < score; _i++)
+            {
+                if (snake[0].Location == snake[_i].Location)
+                {
+                    //for (int _j = _i; _j <= score; _j++)
+                    //    this.Controls.Remove(snake[_j]);
+                    //score = score - (score - _i + 1);
+                    this.Text = "Score: " + score;
+                }
+            }
+        }
+
+        private void _checkBorders()
+        {
+            if (snake[0].Location.X < 0)
+            {
+                game_timer.Stop();
+                this.Text = "Вы вышли за границы";
+            }
+            if (snake[0].Location.X > this.Height)
+            {
+                game_timer.Stop();
+                this.Text = "Вы вышли за границы";
+            }
+            if (snake[0].Location.Y < 0)
+            {
+                game_timer.Stop();
+                this.Text = "Вы вышли за границы";
+            }
+            if (snake[0].Location.Y > this.Height)
+            {
+                game_timer.Stop();
+                this.Text = "Вы вышли за границы";
+            }
+        }
+        private void game_timer_Tick(object sender, EventArgs e)
+        { 
+            move_snake();
+            intersect_fruit();            
+            _checkBorders();
+        }
+        
+        private void intersect_fruit()
+        {
+            if (snake[0].Bounds.IntersectsWith(fruit.Bounds))
+            {
+                //this.Controls.Remove(fruit);
+                ////snake[score].Location = new Point(snake[score].Location.X + scale * dirX, snake[score].Location.Y - scale * dirY);
+                //generate_fruit();
+                //score++;
+
+                //this.Controls.Add(snake[score]);
+                //this.Text = "Счёт:" + " " + score;
+
+               
+
+                this.Text = "Score: " + score++;
+                snake[score] = new PictureBox();
+                snake[score].Location = new Point(snake[score -1].Location.X + 20 * dirX , snake[score - 1].Location.Y - 20 * dirY);
+                snake[score].Size = new Size(scale - 1, scale - 1);
+                snake[score].BackColor = Color.Green;
+                this.Controls.Add(snake[score]);
+                generate_fruit();
+
+            }
+        }
+
+        private void начатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game_timer.Start();
+        }
+
+        private void паузаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game_timer.Stop();
+        }
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void легкоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game_timer.Interval = 300;
+        }
+
+        private void среднеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game_timer.Interval = 200;
+        }
+
+        private void тяжелоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game_timer.Interval = 100;
+        }
+
+        private void холодныеЦветаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fruit_color = Color.Blue;
+            for(int i=0;i<400;i++)
+            {
+                snake[i].BackColor = Color.Yellow;
+            }
+        }
+
+        private void тёплыеЦветаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fruit_color = Color.Pink;
+            for (int i = 0; i < 400; i++)
+            {
+                snake[i].BackColor = Color.Orange;                    ;
+            }
+        }
+
+        private void чёрнобелыеЦветаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fruit_color = Color.Gray;
+            for (int i = 0; i < 400; i++)
+            {
+                snake[i].BackColor = Color.Black; 
+            }
+        }
+
+        private void правилаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("•Игра змейка" +
+                Environment.NewLine+ "•Управление стрелочками" +
+                Environment.NewLine+ "•Змейка не может проходить через стенки" +
+                Environment.NewLine+ "•Для начала игры можно нажать CTRL+N" +
+                Environment.NewLine+ "•Приятной игры!", "Правила игры");
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void generate_grad()
+        {
+            for (int i = 0; i <= scale; i++)
+            {
+                gradx[i] = new PictureBox();
+                gradx[i].Location = new Point(0, i * scale + indent);
+                gradx[i].Size = new Size(width, 1);
+                gradx[i].BackColor = Color.DarkGray;
+                this.Controls.Add(gradx[i]);
+            }
+        }
+
+
+        private void generate_fruit()
+        {
+            Random random = new Random();
+            int r1, r2;
+
+            r1 = random.Next(0, scale);
+            r2 = random.Next(0, scale);
+
+            fruit.Location = new Point(r1 * scale, r2 * scale + indent);
+            fruit.Size = new Size(scale, scale);
+            fruit.BackColor = fruit_color;
+
+            this.Controls.Add(fruit);
+        }
+        
+        private void move_snake()
+        {
+            snake[0].Location = new Point(snake[0].Location.X + dirX * scale, snake[0].Location.Y + dirY * scale);
+            //for (int i = score; i > 0; i--)
+            //{
+            //    snake[i].Location = new Point(snake[i - 1].Location.X, snake[i - 1].Location.Y);
+            //}
+            for (int i = score; i >= 1; i--)
+            {
+                snake[i].Location = snake[i - 1].Location;
+            }
+            if (goup)
+            {
+                dirY = -1;
+                dirX = 0;
+                
+
+            }
+            else if (godown)
+            {
+                dirY = 1;
+                dirX = 0;
+              
+            }
+
+            else if (goleft)
+            {
+                dirX = -1;
+                dirY = 0;
+               
+            }
+
+            else if (goright)
+            {
+                dirX = 1;
+                dirY = 0;
+             
+            }
+
+            _eatItself();
+
+        }
+
+
+    }
+}
